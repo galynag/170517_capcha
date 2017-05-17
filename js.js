cf6eed118fd08d2aa2febd54d29ad26e419f6d6e
galynag <g.alenicheva@gmail.com>
2017-05-17 12:08:17 +0300

    v2

--- js.js
+++ js.js
@@ -1,3 +1,37 @@
-/**
- * Created by UpscaleAcademy on 17.05.2017.
- */
+var massCapcha=[
+    {
+        'counter':0,
+    }
+    {
+        'key': 1231,
+        'imgSrc': '',
+    },
+    {
+        'key': 1231,
+        'imgSrc': '',
+    },
+    {
+        'key': 1231,
+        'imgSrc': '',
+    },
+    {
+        'key': 1231,
+        'imgSrc': '',
+    },
+    {
+        'key': 1231,
+        'imgSrc': '',
+    },
+    {
+        'key': 1231,
+        'imgSrc': '',
+    },
+];
+function getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min)) + min;
+};
+function imgCapcha() {
+    var i;
+    i=getRandomInt(0, massCapcha.length);
+    document.querySelector('#capcha').style.background=
+}
